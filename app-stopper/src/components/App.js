b2b2d1d6967c29fabab9f3cbde622d8da250b9ab
ligayan <daniel.nowosad89@gmail.com>
2019-09-27 22:11:37 +0200

    stopper

--- app-stopper/src/components/App.js
+++ app-stopper/src/components/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
-
 import "./App.css";
+import SwitchButton from "./SwitchButton";
 
 class App extends Component {
+  state = {
+    time: 0,
+    active: false
+  };
+
+  handleclick = () => {
+    if (this.state.active) {
+      clearInterval(this.idInterval);
+    } else {
+      this.idInterval = setInterval(() => this.addSecond(), 1000);
+    }
+    this.setState({ active: !this.state.active });
+  };
+
+  addSecond = () => {
+    this.setState({ time: this.state.time + 1 });
+  };
   render() {
-    return <div>Aplikacja</div>;
+    return (
+      <>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleclick} active={this.state.active} />
+      </>
+    );
   }
 }
 
