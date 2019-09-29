18ddeeea91c093ebbad8d0df178f822c60654884
ligayan <daniel.nowosad89@gmail.com>
2019-09-29 18:53:54 +0200

    Buttons

---
+++ app-fifacalc/build/precache-manifest.1780a3fbef6ad3a93824299c26df5fdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665bb7ea9c24c4e076ed5b8efc38f963",
+    "url": "/Kalkulator_FUT/index.html"
+  },
+  {
+    "revision": "b534816651fecf1e8103",
+    "url": "/Kalkulator_FUT/static/css/main.729573c3.chunk.css"
+  },
+  {
+    "revision": "edd757b5eab718a3a63f",
+    "url": "/Kalkulator_FUT/static/js/2.33367fcc.chunk.js"
+  },
+  {
+    "revision": "b534816651fecf1e8103",
+    "url": "/Kalkulator_FUT/static/js/main.8e161a18.chunk.js"
+  },
+  {
+    "revision": "3df38b588e861603b663",
+    "url": "/Kalkulator_FUT/static/js/runtime-main.38e802e0.js"
+  }
+]);
